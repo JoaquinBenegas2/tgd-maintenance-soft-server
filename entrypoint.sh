4fbbd45ec3ad9a92b4b94b91b2ac3ad7b95323dc
Benegas Joaquin Ezequiel <joacobenegas2@gmail.com>
2025-05-29 18:48:21 -0300

    fix: add shebang to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 envsubst < /app/application-dev.template.properties > /app/application-dev.properties
 
 exec java -Dspring.profiles.active=dev -Dspring.config.additional-location=file:/app/application-dev.properties -jar /app/application.jar
